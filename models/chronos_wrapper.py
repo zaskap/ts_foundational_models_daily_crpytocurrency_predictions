1db86471cb1cb68499b6a0536313556c6bcfa113
Aswin Karthik <zaskap@gmail.com>
2025-04-26 22:30:20 -0400

    Modified chronos wrapper for exogenous variables inclusion

--- models/chronos_wrapper.py
+++ models/chronos_wrapper.py
@@ -10,7 +10,7 @@ class ChronosWrapper:
         self.model = ChronosForecaster(**self.model_params)
         print(f"Model used: ChronosForecaster\nforecasting_horizon={self.forecasting_horizon}\n{self.model.get_params()}")
 
-    def forecast(self, history_data, frequency=None, future_index=None, X_future=None):
+    def forecast(self, history_data, frequency=None, X_exog = None, future_index=None, X_future=None):
         """Forecasts future values using the Chronos model.
 
         Parameters
@@ -24,7 +24,7 @@ class ChronosWrapper:
         -------
         pd.Series or pd.DataFrame: Point forecasts.
         """
-        self.model.fit(history_data, X=None)
+        self.model.fit(history_data, X=X_exog)
 
         if future_index is not None:
             fh = ForecastingHorizon(future_index, is_relative=False)
@@ -42,3 +42,33 @@ class ChronosWrapper:
 
 # point_forecast_chronos, _ = chronos_forecaster.forecast(history_data_single)
 # print("Chronos Point Forecast:", point_forecast_chronos)
+
+# # Sample time series data with exogenous variables
+# chronos_forecaster = ChronosWrapper(forecasting_horizon=7, model_params = {"model_path":"amazon/chronos-t5-tiny"})
+# index = pd.date_range("2023-01-01", periods=200, freq="D")
+# history_data_multi = pd.DataFrame({
+#     "target": np.random.randn(200).cumsum(),
+#     "exog1": np.random.randn(200)
+# }, index=index)
+# point_forecast_chronos_multi, _ = chronos_forecaster.forecast(history_data_multi)
+# print("Chronos Multi-Variable Point Forecast:", point_forecast_chronos_multi)
+
+
+# # Sample time series data with exogenous variables and future index
+# chronos_forecaster = ChronosWrapper(forecasting_horizon=7, model_params = {"model_path":"amazon/chronos-t5-tiny"})
+# index = pd.date_range("2023-01-01", periods=200, freq="D")
+# history_data_multi = pd.DataFrame({
+#     "target": np.random.randn(200).cumsum(),
+#     "exog1": np.random.randn(200),
+#     "exog2": np.random.randn(200)
+# }, index=index)
+
+# point_forecast_chronos_future_without_exog, _ = chronos_forecaster.forecast(history_data_multi["target"])
+# print("Chronos without exogenous variable Point Forecast with Future Index:", point_forecast_chronos_future_without_exog)
+
+# point_forecast_chronos_future_with_exog, _ = chronos_forecaster.forecast(history_data_multi["target"], X_exog=history_data_multi["exog1"])
+# print("Chronos with exogenous variable Point Forecast with Future Index:", point_forecast_chronos_future_with_exog)
+
+# point_forecast_chronos_future_with_multiple_exog, _ = chronos_forecaster.forecast(history_data_multi["target"], X_exog=history_data_multi[["exog1", "exog2"]])
+# print("Chronos with Multiple exogenous variable Point Forecast with Future Index:", point_forecast_chronos_future_with_multiple_exog)
+
